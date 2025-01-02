205defae460a91ebdbc36c1cf5b4fc2943ef405c
Kai Mast <kai@kaimast.com>
2025-01-03 10:48:34 +1300

    Add main function macro

--- executor-macros/src/lib.rs
+++ executor-macros/src/lib.rs
@@ -51,3 +51,39 @@ pub fn test(_args: TokenStream, item: TokenStream) -> TokenStream {
 
     result.into()
 }
+
+#[proc_macro_attribute]
+pub fn main(_args: TokenStream, item: TokenStream) -> TokenStream {
+    let mut input: ItemFn = match syn::parse2(item.clone().into()) {
+        Ok(it) => it,
+        Err(e) => return token_stream_with_error(item, e),
+    };
+
+    if input.sig.asyncness.is_none() {
+        panic!("the `async` keyword is missing from the main function declaration");
+    }
+
+    input.sig.asyncness = None;
+
+    let body = &input.block;
+    let brace_token = input.block.brace_token;
+
+    let tokio_expr = quote! {
+        tokio_uring_executor::initialize();
+        tokio_uring_executor::block_on(async {
+            #body
+        })
+    };
+
+    input.block = syn::parse2(quote! {
+        {
+            #tokio_expr
+        }
+    })
+    .expect("Parsing failure");
+    input.block.brace_token = brace_token;
+
+    quote!{
+        #input
+    }.into()
+}
