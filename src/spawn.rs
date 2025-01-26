f73f595be4ed4db5f4c7c0cd84d0e5719338fff9
Kai Mast <kai@kaimast.com>
2025-01-25 20:32:00 -0800

    Add wrapped to spawn ring

--- src/spawn.rs
+++ src/spawn.rs
@@ -3,9 +3,13 @@ use std::sync::Arc;
 
 use crate::runtime::{FutureWith, JoinHandle, RuntimeInner, ACTIVE_RUNTIME};
 
+/// A spawn ring allows to load balance tasks across all
+/// worker threads in a round-robin fashion
+/// At every spawn call it will move to the next worker thread
 pub struct SpawnRing {
     inner: Arc<RuntimeInner>,
     thread_idx: usize,
+    wrapped: bool,
 }
 
 impl SpawnRing {
@@ -13,7 +17,14 @@ impl SpawnRing {
         Self {
             inner,
             thread_idx: 0,
+            wrapped: false,
         }
+    }
+   
+    // Have we spawned a task on all threads already and
+    // the position got reset to zero?
+    pub fn has_wrapped(&self) -> bool {
+        self.wrapped
     }
 
     pub fn spawn<O: Send + Sized, F: Future<Output = O> + Send + 'static>(
@@ -32,8 +43,12 @@ impl SpawnRing {
     }
 
     fn advance(&mut self) {
-        let num_worker_threads = self.inner.get_thread_count();
-        self.thread_idx = (self.thread_idx + 1) % num_worker_threads;
+        self.thread_idx += 1;
+        
+        if self.thread_idx >= self.inner.get_thread_count() {
+            self.wrapped = true;
+            self.thread_idx = 0;
+        }
     }
 }
 
