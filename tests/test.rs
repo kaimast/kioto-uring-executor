2d5cb171a14647871bf198a715f9b3e2013e3da7
Kai Mast <kai@kaimast.com>
2025-03-01 15:39:24 -0800

    Add runtime handles

--- tests/test.rs
+++ tests/test.rs
@@ -5,6 +5,8 @@ use std::sync::mpsc;
 
 #[test]
 fn block_on() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     kioto::block_on(async {
         sleep(Duration::from_millis(10)).await;
         println!("Hello world");
@@ -13,7 +15,23 @@ fn block_on() {
 
 #[test]
 fn num_threads() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    kioto::block_on(async {
+        assert!(kioto::get_runtime_thread_count() > 0);
+        println!("Hello world");
+    })
+}
+
+#[kioto::test]
+async fn num_threads_in_runtime() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    // make sure ACTIVE_RUNTIME is set correctly
+    assert!(kioto::get_runtime_thread_count() > 0);
+
     kioto::block_on_runtime(async {
+        // ensure ACTIVE_RUNTIME is still set
         assert!(kioto::get_runtime_thread_count() > 0);
         println!("Hello world");
     })
@@ -21,6 +39,8 @@ fn num_threads() {
 
 #[test]
 fn block_on_spawn() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     kioto::block_on(async {
         let fut = kioto::spawn_local(async {
             sleep(Duration::from_millis(10)).await;
@@ -33,6 +53,8 @@ fn block_on_spawn() {
 
 #[test]
 fn runtime_block_on() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     let runtime = kioto::Runtime::new();
     runtime.block_on_with(|| {
         Box::pin(async {
@@ -44,6 +66,8 @@ fn runtime_block_on() {
 
 #[test]
 fn spawn() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     let runtime = kioto::Runtime::new();
     let (sender, receiver) = mpsc::channel();
 
@@ -59,6 +83,8 @@ fn spawn() {
 
 #[kioto::test]
 async fn join() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     let hdl = kioto::spawn_with(|| {
         Box::pin(async move {
             sleep(Duration::from_millis(10)).await;
@@ -71,6 +97,8 @@ async fn join() {
 
 #[kioto::test]
 async fn spawn_local() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     let hdl = kioto::spawn_local(async move {
         sleep(Duration::from_millis(10)).await;
         "Hello world".to_string()
@@ -81,6 +109,8 @@ async fn spawn_local() {
 
 #[test]
 fn spawn_with() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     let runtime = kioto::Runtime::new();
     let (sender, receiver) = mpsc::channel();
 
@@ -96,6 +126,8 @@ fn spawn_with() {
 
 #[kioto::test]
 async fn spawn_ring() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     let mut ring = kioto::new_spawn_ring();
     let hdl = ring.spawn_with(|| {
         Box::pin(async move {
@@ -109,6 +141,8 @@ async fn spawn_ring() {
 
 #[kioto::test]
 async fn executor_macro() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     sleep(Duration::from_millis(10)).await;
     println!("Hello world");
 }
